2659b73a784c9b0396e7b8ce4e5915758ab9d4c5
fishu <pole.fishu@gmail.com>
2019-12-31 10:07:16 +0800

    enablePersistence: repl-diskless-sync = yes

--- pkg/apis/redis/v1beta1/validate.go
+++ pkg/apis/redis/v1beta1/validate.go
@@ -73,7 +73,7 @@ func enablePersistence(config map[string]string) {
 	setConfigMapIfNotExist("auto-aof-rewrite-min-size", "536870912", config)
 	setConfigMapIfNotExist("auto-aof-rewrite-percentage", "100", config)
 	setConfigMapIfNotExist("repl-backlog-size", "62914560", config)
-	setConfigMapIfNotExist("repl-diskless-sync", "no", config)
+	setConfigMapIfNotExist("repl-diskless-sync", "yes", config)
 	setConfigMapIfNotExist("aof-load-truncated", "yes", config)
 	setConfigMapIfNotExist("stop-writes-on-bgsave-error", "no", config)
 	setConfigMapIfNotExist("save", "900 1 300 10", config)
